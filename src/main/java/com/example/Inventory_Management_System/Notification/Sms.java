fd84a5cdcd0d739e7aa19adbfc8fa578d7e4c47c
darshit <darshittarsariya01@gmail.com>
2025-07-25 18:04:27 +0530

    handled sms exception

--- src/main/java/com/example/Inventory_Management_System/Notification/Sms.java
+++ src/main/java/com/example/Inventory_Management_System/Notification/Sms.java
@@ -3,13 +3,18 @@ import com.twilio.Twilio;
 import com.twilio.converter.Promoter;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Value;
+
 import java.net.URI;
 import java.math.BigDecimal;
 public class Sms {
     // Find your Account Sid and Token at twilio.com/console
+    @Value("$twilio.account.sid")
+    private String ACCOUNT_SID;
+    @Value("$twilio.auth.token")
+    private String AUTH_TOKEN;
     public  void paymentSmsSucces(String name,String productName, double price) {
-        twilio.account-sid=${TWILIO_ACCOUNT_SID}
-        twilio.auth-token=${TWILIO_AUTH_TOKEN}
+
 
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         System.out.println("sms");
